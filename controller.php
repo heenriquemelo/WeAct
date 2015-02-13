8e35a62dc6b27025ecc036fc186c9cb950793ac0
Anderson Pimentel <andbrain@gmail.com>
2015-02-13 19:08:58 -0400

    Add conception of json book

--- controller.php
+++ controller.php
@@ -1,9 +1,28 @@
 <?php
-//catch post information 
-$email = file_get_contents("php://input");
 
-//create a file to retrieve user information
-$fp = fopen("emails.json", "a+");
-fwrite($fp, $email);
-fwrite($fp, "\n");	
-fclose($fp);
+function getContactBook($json_file){
+	$json = file_get_contents($json_file);
+	
+	return json_decode($json, true);
+}
+
+function saveContactBook($book_path,$array){
+	$json = json_encode($array);
+	$fp = fopen($book_path, "w");
+	fwrite($fp, $json);
+	fclose($fp);
+}
+
+$book_path = "file.json";
+//catch post information 
+$data = json_decode(file_get_contents("php://input"));
+
+//get all emails
+$book = getContactBook($book_path);
+
+//add new email into the book
+$book[] = $data;
+
+//save all your book in json file
+saveContactBook($book_path, $book);
+?>
