c79ddd48ce7717d2e98738de7103ab2a2bfe5893
Anderson Pimentel <andbrain@gmail.com>
2015-02-26 10:57:49 -0400

    Suggestions about htaccess

--- controller.php
+++ controller.php
@@ -1,23 +1,36 @@
 <?php
 
+function getContext(){
+	$username = "henrique";
+	$password = "12345";
+	$key = $username . ":" . $password;
+
+	$context = stream_context_create(array (
+    'http' => array (
+        	'header' => 'Authorization: Basic ' . base64_encode($key)
+    	)
+	));
+
+	return $context;
+}
+
 function getContactBook($json_file){
-	$json = file_get_contents($json_file);
+	$json = file_get_contents($json_file, false, getContext());
 	
 	return json_decode($json, true);
 }
 
 function saveContactBook($book_path,$array){
 	$json = json_encode($array);
-	$fp = fopen($book_path, "w");
+	$fp = fopen($book_path, "w", false, getContext());
 	fwrite($fp, $json);
 	fwrite($fp, "\n");
 	fclose($fp);
 }
 
-$book_path = "file.json";
+$book_path = "db/file.json";
 //catch post information 
 $data = json_decode(file_get_contents("php://input"));
-
 //get all emails
 $book = getContactBook($book_path);
 
